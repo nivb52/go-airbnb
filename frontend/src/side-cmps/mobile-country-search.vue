7eb0281a22125cc9e3f0e71d931b68b2254e2872
nivb52 <nivb52@gmail.com>
2023-02-17 07:52:00 +0200

    FIX: error using v-model

--- frontend/src/side-cmps/mobile-country-search.vue
+++ frontend/src/side-cmps/mobile-country-search.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="mobile-country-search">
-    <div v-if="activeFilter !== 'mobileCountrySearch'" @click="$emit('setActiveFilter', 'mobileCountrySearch')"
-      class="preview-filter flex-box space-between">
+    <div
+      v-if="activeFilter !== 'mobileCountrySearch'"
+      @click="$emit('setActiveFilter', 'mobileCountrySearch')"
+      class="preview-filter flex-box space-between"
+    >
       <span class="first">Where</span>
       <span v-if="!country" class="second">Add destination</span>
       <span v-else class="second">{{ country }}</span>
@@ -9,16 +12,40 @@
     <div v-else class="country-search active">
       <div class="title">Where to?</div>
       <div class="search-mobile">
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
-          focusable="false" style="display: block; height: 16px; width: 16px;  color: rgb(34, 34, 34);">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          role="presentation"
+          focusable="false"
+          style="
+            display: block;
+            height: 16px;
+            width: 16px;
+            color: rgb(34, 34, 34);
+          "
+        >
           <path
             d="M13 0c7.18 0 13 5.82 13 13 0 2.868-.929 5.519-2.502 7.669l7.916 7.917-2.828 2.828-7.917-7.916A12.942 12.942 0 0 1 13 26C5.82 26 0 20.18 0 13S5.82 0 13 0zm0 4a9 9 0 1 0 0 18 9 9 0 0 0 0-18z"
-            opacity=".8"></path>
+            opacity=".8"
+          ></path>
         </svg>
-        <input v-focus t v-model="country" @input="getCountryList" type="search" placeholder="Search destinations">
+        <input
+          v-focus
+          v-bind="country"
+          @input="getCountryList"
+          type="search"
+          placeholder="Search destinations"
+        />
       </div>
-      <country-modal @setFilterSearch="setFilterSearch" v-if="!isListModalShown"></country-modal>
-      <search-list-modal v-else @setInputBySearch="setInputBySearch"></search-list-modal>
+      <country-modal
+        @setFilterSearch="setFilterSearch"
+        v-if="!isListModalShown"
+      ></country-modal>
+      <search-list-modal
+        v-else
+        @setInputBySearch="setInputBySearch"
+      ></search-list-modal>
     </div>
   </section>
 </template>
@@ -31,31 +58,31 @@ export default {
   data() {
     return {
       isListModalShown: false,
-      isCountryModalShown: false
-    }
+      isCountryModalShown: false,
+    };
   },
   props: {
     activeFilter: String,
-    country: String
+    country: String,
   },
   mounted() {
-    this.getCountryList = utilService.debounce(this.getCountryList)
+    this.getCountryList = utilService.debounce(this.getCountryList);
   },
   methods: {
     setFilterSearch(region) {
-      this.$emit('setFilterSearch', region.name)
+      this.$emit('setFilterSearch', region.name);
     },
     getCountryList() {
-      this.isListModalShown = (this.country === '') ? false : true
-      this.$store.dispatch({ type: 'getCountryList', txt: this.country })
+      this.isListModalShown = this.country === '' ? false : true;
+      this.$store.dispatch({ type: 'getCountryList', txt: this.country });
     },
     setInputBySearch(input) {
-      this.$emit('setFilterSearch', input)
-    }
+      this.$emit('setFilterSearch', input);
+    },
   },
   components: {
     countryModal,
-    searchListModal
-  }
-}
+    searchListModal,
+  },
+};
 </script>
